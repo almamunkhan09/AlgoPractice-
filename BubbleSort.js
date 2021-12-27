566f254b385bdcc96c9c53f90ab7ff2f9a335e9f
Al Mamun Khan <almamunkhan09@gmail.com>
2021-12-27 10:56:36 +0100

    Bubble sort finalized

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,24 +1,31 @@
-// Writing  a function that will sort numbers using bubble sort Algorithm
-
-
-const bubbleSort = (array)=>{
-
-    for (let k = 0; k < array.length;k++){
-        for (let i = 1; i<array.length; i++){
-            if(array[i]<array[i-1]){
-                
-                const temp = array[i-1];
-                array[i-1] = array[i];
-                array[i] = temp;
+function bubbleSort(array) {
     
-    
-            }
-    
-        }
-    }
-
-return array;
-
-}
-const x = bubbleSort([4,3,2,1]);
-console.log(x);
+      
+  //let count  = 0; need to check if the algorithm works perfectly and dont run the second loop  when its already sorted
+  
+      for (let k = 0; k < array.length;k++){
+          
+          //count++;
+          
+          let swapped = 0;
+          for (let i = 1; i<array.length; i++){
+              if(array[i]<array[i-1]){
+                  
+                  const temp = array[i-1];
+                  array[i-1] = array[i];
+                  array[i] = temp;
+                  
+                  swapped++;
+      
+      
+              }
+      
+          }
+          
+          if (swapped === 0) break;
+      }
+  //console.log(count);
+  return array;
+  }
+  
+  console.log(bubbleSort([8,9,2,3,4,5,1,8,9,10,11]));
