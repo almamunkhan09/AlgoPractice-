800e30f245f0112ec2be320e03d1d4f300c076c4
Al Mamun Khan <almamunkhan09@gmail.com>
2021-12-30 18:36:22 +0100

    Finished the insertion sort Problem

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,17 +1,33 @@
-const insertionSort = array=>{
+const insertionSort = array =>{
 
 
-    for (let i =0;i<array.length;i++){
+    for (let i =1;i<array.length;i++){
 
+        let temp = array[i];
+        let currentPosition  = i;
+        let j = i;
+        while(!(j<0)){
 
-        
-
-        // for (let j=0;j<array.length;j++){
+            if(temp<array[j-1]){
+                array[j] = array[j-1];
+            }
             
-        // }
+            else break;
+
+            j-=1;
+            
+
+        }
+        currentPosition = j;
+        array[currentPosition] = temp;
         
 
     }
+
+    return array;
+
+    
+    
 }
 
-console.log(insertionSort());
+console.log(insertionSort([4,2,1,5,1,2,7,8,9,10]));
