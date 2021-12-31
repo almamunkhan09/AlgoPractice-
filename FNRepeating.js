da94494babdd34fd8841e6312d0bf0aa05c6c758
Al Mamun Khan <almamunkhan09@gmail.com>
2021-12-31 09:54:30 +0100

    Solved if a charecter is a number

--- FNRepeating.js
+++ FNRepeating.js
@@ -1,30 +1,35 @@
 function firstNonRepeatingCharacter(string) {
-
-    string =string+'0';
+    
+    string =string +'$';
 
 	const length = string.length;
+    
     let result = -1;
+
+    
 	
 	for(let i=0;i<length-1;i++){
 		const str = string.slice(i+1);
 		
-		if(str.includes(string[i]) )
-        {
-           string = string.replaceAll(string[i],0);
+		if(str.includes(string[i]) ){
+
+           string = string.replaceAll(string[i],"$");
+           
+           
         }
 
-
-			
-			
-		
-		
-		else {result = string.indexOf(string[i]);break;}
+        
+		else {
+            result = string.indexOf(string[i]);
+            break;
+        }
+        
 	}
   // Write your code here.
-
-  console.log(string);
+  
+  
   return result;
 }
 
 
-console.log(firstNonRepeatingCharacter("ababccd"));
+console.log(firstNonRepeatingCharacter("00fabab"));
